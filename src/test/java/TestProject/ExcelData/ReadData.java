a1fe6f52ac728bb03895a32897c99a0871c4dc1c
Sagar <sagarw094@gmail.com>
2025-04-19 12:49:37 +0530

    My first github project

---
+++ src/test/java/TestProject/ExcelData/ReadData.java
@@ -0,0 +1,30 @@
+package TestProject.ExcelData;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class ReadData {
+    
+	@Test
+	public void excelDatatest() throws IOException {
+		// TODO Auto-generated method stub
+		
+		FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"/src/test/Data.xlsx");
+        
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+		
+		XSSFSheet sheet = workbook.getSheet("Sheet1");
+		
+		int rows = sheet.getLastRowNum();
+		int cell = sheet.getRow(1).getLastCellNum();
+		
+		System.out.println(rows +"  "+cell);
+		
+		
+	}
+	
+}
